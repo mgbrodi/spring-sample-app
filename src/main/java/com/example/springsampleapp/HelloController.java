9d8388429a146a9ac0c8fa27663dbba02d64114f
Maria Gabriella Brodi <50110012+mgbrodi@users.noreply.github.com>
2020-08-22 17:19:22 -0400

    wip

--- src/main/java/com/example/springsampleapp/HelloController.java
+++ src/main/java/com/example/springsampleapp/HelloController.java
@@ -19,7 +19,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String hello() {
-		return "Hello, Prasanna!! MAybe now?  " + toString();
+		return "Hello, Prasanna!! Maybe now?  " + toString();
 	}
 
 	@Override
