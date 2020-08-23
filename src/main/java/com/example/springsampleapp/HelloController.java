6da0ae5731265e84335014d8e4c109cf12815e35
Maria Gabriella Brodi <50110012+mgbrodi@users.noreply.github.com>
2020-08-23 15:35:37 -0400

    Update HelloController.java

--- src/main/java/com/example/springsampleapp/HelloController.java
+++ src/main/java/com/example/springsampleapp/HelloController.java
@@ -19,7 +19,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String hello() {
-		return "Hello, Prasanna!! Ciao Gabry!! :)" + toString();
+		return "Hello, Prasanna!! Ciao Gabry!! :-P" + toString();
 	}
 
 	@Override
